bd5f6ec1a0228d029db7f7bf95fef0441128cb9e
kunal parikh <jskr1313@gmail.com>
2018-10-09 02:11:05 +0530

    xx
    xx

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/index.html',
+       '/csv.html',
+
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
